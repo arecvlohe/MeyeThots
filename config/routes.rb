946125f849fbc504ee81abb5b5d6a96b436ec08f
Adam Recvlohe <adam.recvlohe@gmail.com>
2015-05-24 12:50:41 -0400

    Modify Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  root 'static_pages#home'
+  get 'static_pages/help', to: 'static_pages#help'
+  get 'static_pages/about', to: 'static_pages#about'
 
 end
