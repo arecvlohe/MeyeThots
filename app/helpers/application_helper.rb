e09d90fea4e0bb8bc38e5380381c698c42f4a58d
Adam Recvlohe <adam.recvlohe@gmail.com>
2015-05-24 14:33:55 -0400

    Finish Layout, Add Styles, Start User Sign Up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns full title if no title is on page
+  def full_title(page_title = '')
+    base_title = "MeyeThots"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/help'   , to: 'static_pages#help'
-  get 'static_pages/about'  , to: 'static_pages#about'
-  get 'static_pages/contact', to: 'static_pages#contact'
+  get 'users/new'
 
+  root 'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 end
